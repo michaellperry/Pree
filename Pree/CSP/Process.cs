99955017a368767d25fccda172813f130fd47d25
Michael L Perry <michael@qedcode.com>
2014-12-16 10:09:03 -0600

    Added Process base class for simple messaging between processes.

---
+++ Pree/CSP/Process.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpdateControls.Fields;
+
+namespace Pree.CSP
+{
+    class Process
+    {
+        private AsyncSemaphore _lock = new AsyncSemaphore();
+        private Independent<Exception> _exception = new Independent<Exception>();
+
+        public Independent<Exception> Exception
+        {
+            get
+            {
+                lock (_exception)
+                {
+                    return _exception;
+                }
+            }
+        }
+
+        protected async void Enqueue(Func<Task> request)
+        {
+            await _lock.WaitAsync();
+            try
+            {
+                await request();
+                lock (_exception)
+                {
+                    _exception.Value = null;
+                }
+            }
+            catch (Exception x)
+            {
+                lock (_exception)
+                {
+                    _exception.Value = x;
+                }
+            }
+            finally
+            {
+                _lock.Release();
+            }
+        }
+
+        protected void Enqueue(Action request)
+        {
+            Enqueue(() => Task.Run(request));
+        }
+    }
+}
